5318cf5a3ffdff365e8491ab0d9e2d7d91fa56cc
Carol Victor <142908008+Carolitta-11@users.noreply.github.com>
2026-08-05 16:14:19 +0300

    Update note display logic and message
    Refactor note opening functionality and add message.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-document.getElementById("openNote").addEventListener("click",()=>{
+const homeCard = document.getElementById("homeCard");
 
-alert("Coming Soon 🤍");
+const noteCard = document.getElementById("noteCard");
 
-});
+const noteText = document.getElementById("noteText");
+
+document.getElementById("openNote").onclick = function(){
+
+    homeCard.style.display = "none";
+
+    noteCard.style.display = "block";
+
+    noteText.innerHTML =
+    `Sometimes life becomes lighter just because you're in it. 🌼`;
+}
+
+document.getElementById("backBtn").onclick = function(){
+
+    noteCard.style.display = "none";
+
+    homeCard.style.display = "block";
+
+}
